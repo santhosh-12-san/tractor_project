c7bd0a537a9ff28c9d0f95c74424e1e72e85a74c
SATISHSP-05 <satishsp0503@gmail.com>
2026-07-16 04:10:45 +0530

    upgrade python to 3.13

--- tractor_management/customers/models.py
+++ tractor_management/customers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import Group, Permission
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 
@@ -20,6 +21,9 @@ class User(AbstractUser):
     ]
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Owner')
     customer_profile = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True, related_name='users')
+    # Override the ManyToMany fields from PermissionsMixin to avoid reverse accessor name clashes
+    groups = models.ManyToManyField(Group, related_name='custom_user_groups', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions', blank=True)
 
     def __str__(self):
         return f"{self.username} ({self.role})"
